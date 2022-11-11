5af1964f9d8bc955f0403c04ad081c2d8373f6f1
Joe Bruce <jabruce@dreamport.tech>
2022-11-11 12:20:15 -0500

    Annotated script to indicate importance (or not) of output

--- rpe021_example.py
+++ rpe021_example.py
@@ -3,7 +3,10 @@ Example RPE-021 REST API implementation based on Python and FastAPI. Note that
 JSON schema validation is NOT as tight as in the published schema. Better input
 validation is left to competitors, and WILL NOT be tested during the RPE. Only
 valid JSON data will be sent to competitor solutions.
+
+Copyright 2022, Maryland Innovation and Security Institute
 """
+
 from fastapi import FastAPI, HTTPException
 from datetime import datetime
 from typing import List, Optional
@@ -13,6 +16,7 @@ app = FastAPI()
 
 elements = {}
 
+# Structure for a network interface
 class Interface(BaseModel):
     label: str
     interface_id: str
@@ -20,6 +24,7 @@ class Interface(BaseModel):
     ipv6: str
     mac: str
 
+# Structure for a network element - network, endpoint, or connection
 class Element(BaseModel):
     id: str
     timestamp: datetime
@@ -45,6 +50,7 @@ class Element(BaseModel):
 
 @app.get('/elements')
 def get_all_elements():
+    """Return a list of all elements."""
     elem_list = []
     for key, elem in elements.items():
         elem_list.append(elem)
@@ -52,38 +58,48 @@ def get_all_elements():
 
 @app.get('/elements/{id}')
 def get_element(id: str):
+    """Return a single element, or 404 if ID is not found."""
     element = find_element(id)
     return element
 
 @app.post('/elements')
 def add_element(element: Element):
+    """Add a single element, or 400 if ID already exists."""
     #print('element: ' + str(element))
     id = element.id
     if id not in elements:
         elements[id] = element
+        # NOTE: Output is not significant, just the HTTP response code (200/4xx)
         return element
     else:
         raise HTTPException(status_code=400, detail='Element ID already exists')
 
 @app.put('/elements/{id}')
 def update_element(element: Element):
+    """Update an existing element, or 404 if ID is not found."""
     id = element.id
     origElement = find_element(id)
     elements[id] = element
+    # NOTE: Output is not significant, just the HTTP response code (200/4xx)
     return {"orig_element": origElement, "new_element": element}
 
 @app.delete('/elements')
 def delete_all_elements():
+    """Delete all elements."""
     elements.clear()
+    # NOTE: Output is not significant, just the HTTP response code (200)
     return {'elements': []}
 
 @app.delete('/elements/{id}')
 def delete_element(id: str):
+    """Delete an existing element, or 404 if ID is not found."""
     element = find_element(id)
     del elements[id]
+    # NOTE: Output is not significant, just the HTTP response code (200/404)
     return element
 
 def find_element(id: str):
+    """Helper function to look up an element by ID or throw a HTTP 404 response."""
     if id in elements:
         return elements[id]
     else:
